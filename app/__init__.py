6932302dad3e643c67cd6d055206e9f1c872789e
Tom Carsello <tkcarsello@gmail.com>
2019-12-11 20:50:56 -0600

    Completed through Chapter 9

--- app/__init__.py
+++ app/__init__.py
@@ -13,4 +13,19 @@ from flask_login import LoginManager
 login=LoginManager(app)
 login.login_view='login'
 
-from app import routes, models
+from app import routes, models, errors
+
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
